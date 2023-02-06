d7cad6fe442b4ce16ebc5f0c4690d074dd3d0451
Luciano <91077377+Luciano-C@users.noreply.github.com>
2023-02-06 18:05:55 -0300

    predictions django app added. Working on navbar

--- CapitalCall/settings.py
+++ CapitalCall/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authuser'
+    'authuser',
+    'predictions'
 ]
 
 AUTH_USER_MODEL = "authuser.User"
