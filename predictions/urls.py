683cdcaadd12f9d97909ef907ce304240b675a86
Luciano <91077377+Luciano-C@users.noreply.github.com>
2023-02-09 12:52:39 -0300

    Sign in, Sign up and Log out

--- predictions/urls.py
+++ predictions/urls.py
@@ -4,5 +4,9 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
-    path("read-more/", read_more, name="read_more")
+    path("read-more/", read_more, name="read_more"),
+    path("signin/", signin, name="signin"),
+    path("signup/", signup, name="signup"),
+    path("signout/", signout, name="signout"),
+    path("predictions/", predictions, name="predictions")
 ]
--- predictions/views.py
+++ predictions/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .utils.scraper import get_economic_indicators 
+from authuser.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate 
+from django.db import IntegrityError
+
 
 # Create your views here.
 def home(request):
@@ -13,3 +18,45 @@ def home(request):
 
 def read_more(request):
     return render(request, 'read_more.html')
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, 'signin.html')
+    else:
+        user = authenticate(
+            request, email=request.POST['email'], password=request.POST['password']
+        ) 
+        
+        if user is None:
+            return render(request, 'signin.html', {
+                'error': 'Email or password incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('predictions') 
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, 'signup.html')
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(email=request.POST['email'])
+                user.set_password(request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('predictions')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'error': 'Email already exists'
+                })
+            
+@login_required            
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def predictions(request):
+    return render(request, 'predictions.html')
